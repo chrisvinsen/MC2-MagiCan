768f1fa1c7abdb5456cbaf91ce2383fd54d453d0
Christianto Vinsen <christianto.vinsen@student.umn.ac.id>
2022-06-19 21:10:32 +0700

    Dummy Dashboard

--- MagiCan/Views/Screens/TempDashboardViewController.swift
+++ MagiCan/Views/Screens/TempDashboardViewController.swift
@@ -8,9 +8,22 @@
 import UIKit
 
 class TempDashboardViewController: UIViewController {
+    
+    let text = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        text.translatesAutoresizingMaskIntoConstraints = false
+        text.text = "TEMPORARY DASHBOARD"
+        text.textColor = .black
+        
+        view.addSubview(text)
+        
+        NSLayoutConstraint.activate([
+            text.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            text.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
 
         view.backgroundColor = .green
     }
