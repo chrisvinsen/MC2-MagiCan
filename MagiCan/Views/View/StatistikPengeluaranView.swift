4568114794dd51f606d722d341458ab5cfeb0804
Nurul Srianda Putri <nurulsriandaputri@gmail.com>
2022-06-21 22:54:40 +0700

    init view for statistik pemasuka, statistik pengeluaran, & profile view

---
+++ MagiCan/Views/View/StatistikPengeluaranView.swift
@@ -0,0 +1,20 @@
+//
+//  StatistikPengeluaranView.swift
+//  MagiCan
+//
+//  Created by Nurul Srianda Putri on 21/06/22.
+//
+
+import UIKit
+
+class StatistikPengeluaranView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
